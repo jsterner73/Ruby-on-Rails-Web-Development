d3f973a94e319c733ee4a7fc1fdb4194752048f0
Joerg Sterner <joerg.sterner@gmail.com>
2016-01-20 23:09:45 +0100

    merge

--- fullstack-course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/solution/module3_1_assignment.rb
+++ fullstack-course1-module3/Assignments/Lesson03-Assignment01-HTTP-Client/solution/module3_1_assignment.rb
@@ -3,11 +3,16 @@ require 'httparty'
 class Recipe
   include HTTParty
 
-  base_uri "http://www.recipepuppy.com/api"
-  default_params onlyImages: 1
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api"
+
+  default_params key: key_value
   format :json
 
   def self.for (keyword)
-    get("", query: {q: keyword})["results"]
+    get("/search", query: {q: keyword})["recipes"]
   end
 end
+
+print Recipe.for "chocolate"
